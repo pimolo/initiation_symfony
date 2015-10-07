1f34e4757518c70c6ecc4d5e58198e58f8de6ccb
Rémi Andrieux <pimolo10300@gmail.com>
2015-10-07 10:14:56 +0200

    Add page with dynamic background

--- src/AppBundle/Controller/HelloController.php
+++ src/AppBundle/Controller/HelloController.php
@@ -9,11 +9,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController extends Controller
 {
     /**
-     * @Route("/hello")
+     * @Route("/hello", name="app_hello_world")
      * @return Response
      */
     public function helloAction()
     {
         return $this->render('AppBundle::hello.html.twig');
+    }
+
+    /**
+     * @Route("/hello/switch_bg/{color}", name="app_hello_switch_background", defaults={"color"="red"})
+     * @param string $color
+     * @return Response
+     */
+    public function switchBackgroundAction($color)
+    {
+        return $this->render('AppBundle::hello.html.twig', [
+            'color' => $color
+        ]);
     }
 }
